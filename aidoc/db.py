be2c3bc326463e4c3dc66c3dfb8d2ea07b8eaeea
patiwet@eng.cmu.ac.th <patiwet@eng.cmu.ac.th>
2024-05-06 02:15:12 +0700

    first development

---
+++ aidoc/db.py
@@ -0,0 +1,46 @@
+import mysql.connector
+from flask import current_app, g
+import click
+
+def get_db():
+    if 'db' not in g:
+        g.db = mysql.connector.connect(
+            host=current_app.config['DB_HOST'],
+            database=current_app.config['DB_DATABASE'],
+            user=current_app.config['DB_USER'],
+            password=current_app.config['DB_PASSWORD']
+        )
+        g.db.autocommit = True
+    
+    return (g.db, g.db.cursor(dictionary=True))
+
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
+
+@click.command('init-db')
+def init_db():
+    db, cursor = get_db()
+    cursor.execute("SHOW TABLES")
+    if cursor.fetchone() is None:
+        click.echo('Initializing the database: ')
+
+        with open('schema.sql') as f:
+            cursor.execute(f.read(), multi=True)
+        close_db()
+
+        db, cursor = get_db()
+        cursor.execute("SHOW TABLES")
+        if cursor.fetchone() is None:
+            click.echo('... Failed to initialize the database.')
+        else:
+            click.echo('... Successfully.')
+    else:
+        click.echo('The tables already exists.')
+
+  
+def init_app(app):
+    app.teardown_appcontext(close_db)
+    app.cli.add_command(init_db)
